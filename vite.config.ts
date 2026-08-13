f3c3db0db7e5032169f303f21d551189aa9a05ea
Johnny <eitv@MacBook-Pro-de-EiTV.local>
2026-08-13 14:18:41 -0300

    change version embla

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
   plugins: [
     react(),
+
+    tailwindcss(),
 
     legacy({
       targets: ["Chrome 38"],
